7c57ae427cd43db4f7fd1543fbd38006731fe0ca
Aung Moe Kyaw <87358448+AungMoeKyaw135@users.noreply.github.com>
2022-05-01 22:00:51 +0630

    Update main.py

--- main.py
+++ main.py
@@ -93,12 +93,11 @@ def main():
                             menu_icon="cast", default_index=0, orientation="horizontal")
 
     if selected == "Demo":
-       selected0 = option_menu(None, ["Demonstration", "Computer Vision", " Evaluation Metric", ],
+        selected0 = option_menu(None, ["Demonstration", "Computer Vision", " Evaluation Metric", ],
                             icons=['activity', 'eye-fill', "check2-circle"],
                             menu_icon="cast", default_index=0, orientation="horizontal")
        if selected0 == "Demonstration":
            demo()
-
 
 
 
